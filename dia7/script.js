309c49bf53004d2ea1e8fdbd997f5cfd1e6aeecc
Gabriel Vinicius dos Santos <100848560+gabrielvinni@users.noreply.github.com>
2023-04-25 16:51:31 -0300

    desafio 7 finalizado

--- dia7/script.js
+++ dia7/script.js
@@ -1,21 +1,12 @@
-let ativo = 0
-let ptxt = document.getElementById("procurar-txt")
-let btmn = document.getElementById("bt-menu")
-
 function procurar() {
-    if (ativo == 1) {
-        ativo = 0
-        ptxt.style.translate = "20px"
-        ptxt.style.opacity = "0"
-        btmn.style.opacity = "1"
-        ptxt.style.visibility = "hidden"
-        btmn.style.visibility = "visible"
-    } else {
-        ativo = 1
-        ptxt.style.translate = "0px"
-        ptxt.style.opacity = "1"
-        btmn.style.opacity = "0"
-        ptxt.style.visibility = "visible"
-        btmn.style.visibility = "hidden"
-    }
+    var ptxt = document.getElementById("procurar-txt");
+    ptxt.classList.toggle("ativo")
+}
+
+function abrirmenu() {
+    var painel = document.getElementById("painel")
+    var menu = document.getElementById("menu")
+
+    painel.classList.toggle("painel-direita")
+    menu.classList.toggle("menu-esquerda")
 }
